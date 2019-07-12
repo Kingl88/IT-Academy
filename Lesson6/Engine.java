be99c9f58732c2df3abbc7ce6be43b159736325e
Kingl88 <34752938+Kingl88@users.noreply.github.com>
2019-07-12 09:57:49 +0300

    Delete Engine.java

--- Lesson6/Engine.java
+++
@@ -1,8 +0,0 @@
-package by.it_academy.Lesson6;
-
-public class Engine { // Класс двигатель
-    private int powerEngine; // Мощность двигателя.
-    private int numberOfCylinders; // Количество цилиндров.
-
-    public void FuelInjection(){} // Метод впрыска топлива
-}
