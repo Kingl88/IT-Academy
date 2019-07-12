03932def839ea17cdf241b368acdc4b4b03199dd
Kingl88 <34752938+Kingl88@users.noreply.github.com>
2019-07-12 09:57:38 +0300

    Delete Transmission.java

--- Lesson6/Transmission.java
+++
@@ -1,8 +0,0 @@
-package by.it_academy.Lesson6;
-
-public class Transmission { //Класс коробка передач.
-    private int numberOfGears; // Количесвто передач.
-    private int currentGear; // Текущая передача.
-
-    public void switchGear(int currentGear){} // Метод переключения передач.
-}
