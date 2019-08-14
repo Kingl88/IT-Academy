db5012aa148f249d945048b81bf8c776525cff9f
Kingl88 <34752938+Kingl88@users.noreply.github.com>
2019-08-14 12:23:27 +0300

    Add files via upload

---
+++ Lesson10/enums/brand_cars/Bmw.java
@@ -0,0 +1,40 @@
+package by.it_academy.Lesson10.enums.brand_cars;
+
+import by.it_academy.Lesson10.my_interface.InterfaceForEnum;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+public enum Bmw implements Serializable, InterfaceForEnum {
+    X1("X1", 2009, Calendar.getInstance().get(Calendar.YEAR), 1000), X2("X2", 2018, Calendar.getInstance().get(Calendar.YEAR), 2000),
+    X3("X3", 2003, Calendar.getInstance().get(Calendar.YEAR), 3000), X4("X4", 2014, Calendar.getInstance().get(Calendar.YEAR), 4000),
+    X5("X5", 1999, Calendar.getInstance().get(Calendar.YEAR), 5000), X6("X6", 2007, Calendar.getInstance().get(Calendar.YEAR), 6000),
+    X7("X7", 2018, Calendar.getInstance().get(Calendar.YEAR), 7000), I3("i3", 2013, Calendar.getInstance().get(Calendar.YEAR), 8000),
+    I8("i8", 2013, Calendar.getInstance().get(Calendar.YEAR), 9000);
+
+    private String name;
+    private int startOfRelease;
+    private int endOfRelease;
+    private int price;
+
+    Bmw(String name, int startOfRelease, int endOfRelease, int price) {
+        this.name = name;
+        this.startOfRelease = startOfRelease;
+        this.endOfRelease = endOfRelease;
+        this.price = price;
+    }
+
+    public int getStartOfRelease() {
+        return startOfRelease;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getEndOfRelease() {
+        return endOfRelease;
+    }
+
+    public String getName(){ return name;}
+}
---
+++ Lesson10/enums/brand_cars/Porsche.java
@@ -0,0 +1,38 @@
+package by.it_academy.Lesson10.enums.brand_cars;
+
+import by.it_academy.Lesson10.my_interface.InterfaceForEnum;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+public enum Porsche implements Serializable, InterfaceForEnum {
+    _911("911", 1963, Calendar.getInstance().get(Calendar.YEAR), 1000), CAYENNE("Cayenne", 2002, Calendar.getInstance().get(Calendar.YEAR), 2000),
+    CAYMAN("Cayman", 2008, Calendar.getInstance().get(Calendar.YEAR), 3000), PANAMERA("Panamera", 2009, Calendar.getInstance().get(Calendar.YEAR), 4000);
+    private String name;
+    private int startOfRelease;
+    private int endOfRelease;
+    private int price;
+
+    Porsche(String name, int startOfRelease, int endOfRelease, int price) {
+        this.name = name;
+        this.startOfRelease = startOfRelease;
+        this.endOfRelease = endOfRelease;
+        this.price = price;
+    }
+
+    public int getStartOfRelease() {
+        return startOfRelease;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getEndOfRelease() {
+        return endOfRelease;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
