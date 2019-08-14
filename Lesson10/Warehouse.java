96b8cb0d1e28c75af685aa6e24e4332288c344c1
Kingl88 <34752938+Kingl88@users.noreply.github.com>
2019-08-14 12:22:30 +0300

    Add files via upload

---
+++ Lesson10/Warehouse.java
@@ -0,0 +1,52 @@
+package by.it_academy.Lesson10;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Warehouse implements Serializable {
+    private static int uid = 0;
+    private int id;
+    private String name = "Склад №";
+    private ArrayList<Car> cars;
+    private boolean isTradIn;
+    private boolean isFull = false;
+
+    public Warehouse(boolean isTradIn) {
+        setId();
+        setTradIn(isTradIn);
+        setCars();
+    }
+
+    private void setId() {
+        id = ++uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Car> getCars() {
+
+        return cars;
+    }
+
+    public void setCars() {
+        this.cars = new ArrayList<>();
+    }
+
+    public boolean isTradIn() {
+        return isTradIn;
+    }
+
+    public void setTradIn(boolean tradIn) {
+        isTradIn = tradIn;
+    }
+
+    public boolean isFull() {
+        return isFull;
+    }
+
+    public void setFull(boolean full) {
+        isFull = full;
+    }
+}
