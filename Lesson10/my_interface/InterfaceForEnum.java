1b266c79eb0007639a6b9751224f7b56a296ade2
Kingl88 <34752938+Kingl88@users.noreply.github.com>
2019-08-14 12:25:40 +0300

    Add files via upload

--- Lesson10/my_interface/InterfaceForEnum.java
+++ Lesson10/my_interface/InterfaceForEnum.java
@@ -1,1 +1,10 @@
+package by.it_academy.Lesson10.my_interface;
 
+import java.io.Serializable;
+
+public interface InterfaceForEnum extends Serializable {
+    String getName();
+    int getStartOfRelease();
+    int getPrice();
+    int getEndOfRelease();
+}
