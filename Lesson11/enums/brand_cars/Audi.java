f0c022a82852543dc45948c3de8de49ce2998547
Kingl88 <34752938+Kingl88@users.noreply.github.com>
2019-08-19 20:08:45 +0300

    Add files via upload

--- Lesson11/enums/brand_cars/Audi.java
+++ Lesson11/enums/brand_cars/Audi.java
@@ -1,1 +1,39 @@
+package by.it_academy.Lesson11.enums.brand_cars;
 
+import by.it_academy.Lesson11.my_interface.InterfaceForEnum;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+public enum Audi implements Serializable, InterfaceForEnum {
+    A4("A4", 1994, Calendar.getInstance().get(Calendar.YEAR), 1000), A5("A5", 2007, Calendar.getInstance().get(Calendar.YEAR), 2000),
+    A6("A6", 1994, Calendar.getInstance().get(Calendar.YEAR),3000), A7("A7", 2010, Calendar.getInstance().get(Calendar.YEAR), 4000),
+    A8("A8", 1994, Calendar.getInstance().get(Calendar.YEAR), 5000), Q3("Q3", 2011, Calendar.getInstance().get(Calendar.YEAR), 6000),
+    Q5("Q5", 2008, Calendar.getInstance().get(Calendar.YEAR), 7000), Q7("Q7", 2005, Calendar.getInstance().get(Calendar.YEAR), 8000),
+    Q8("Q8", 2018, Calendar.getInstance().get(Calendar.YEAR), 9000), E_TRON("e-tron", 2018, Calendar.getInstance().get(Calendar.YEAR), 10000);
+    private String name;
+    private int startOfRelease;
+    private int endOfRelease;
+    private int price;
+
+    Audi(String name, int startOfRelease, int endOfRelease, int price) {
+        this.name = name;
+        this.startOfRelease = startOfRelease;
+        this.endOfRelease = endOfRelease;
+        this.price = price;
+    }
+
+    public int getStartOfRelease() {
+        return startOfRelease;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getEndOfRelease() {
+        return endOfRelease;
+    }
+
+    public String getName(){ return name;}
+}
---
+++ Lesson11/enums/brand_cars/Mercedes.java
@@ -0,0 +1,40 @@
+package by.it_academy.Lesson11.enums.brand_cars;
+
+import by.it_academy.Lesson11.my_interface.InterfaceForEnum;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+public enum Mercedes implements Serializable, InterfaceForEnum {
+    CLS("CLS", 2004, Calendar.getInstance().get(Calendar.YEAR), 1000), GLA("GLA", 2013, Calendar.getInstance().get(Calendar.YEAR), 2000),
+    SL("SL", 1963, Calendar.getInstance().get(Calendar.YEAR), 3000), SLK("SLK", 1996, 2016, 4000),
+    GLS("GLS", 2015, Calendar.getInstance().get(Calendar.YEAR), 5000);
+
+    private String name;
+    private int startOfRelease;
+    private int endOfRelease;
+    private int price;
+
+    Mercedes(String name, int startOfRelease, int endOfRelease, int price) {
+        this.name = name;
+        this.startOfRelease = startOfRelease;
+        this.endOfRelease = endOfRelease;
+        this.price = price;
+    }
+
+    public int getEndOfRelease() {
+        return endOfRelease;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getStartOfRelease() {
+        return startOfRelease;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
