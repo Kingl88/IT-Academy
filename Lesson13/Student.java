451beccac4451dedc6ea962bcbe7b1ce2fd3bc18
Kingl88 <34752938+Kingl88@users.noreply.github.com>
2019-08-26 00:39:22 +0300

    Add files via upload

--- Lesson13/Student.java
+++ Lesson13/Student.java
@@ -3,28 +3,33 @@ package by.it_academy.Lesson13;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Student {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
+        List<String> listIn;
+        ArrayList<String> listOut = new ArrayList<>();
         String fileName = "C:\\Users\\siarh\\OneDrive\\IdeaProjects\\IT-Academy\\src\\by\\it_academy\\Lesson13\\students.txt";
         try(BufferedReader reader = new BufferedReader(new FileReader(fileName)))
         {
             while (reader.ready()) {
-                String stroka = reader.readLine();
-                String[] strTemp = stroka.split(" ");
-                if(Integer.parseInt(strTemp[1]) > 7){
-                    strTemp[0] = strTemp[0].toUpperCase();
+                String stroka = reader.readLine();//Считываем строку.
+                String[] result = stroka.replaceAll("[\\D*&&[\\S]]", "").trim().split(" ");//Получаем массив только цифр без пробелов.
+                listIn = Arrays.asList(result);//Преобразуем массив к списку.
+                double average = listIn.stream().mapToInt(Integer::parseInt).average().orElseGet(() -> 0.0);//Находим среднее арифметическое.
+                if(average > 7){
+                    listOut.add(stroka.toUpperCase());
+                } else {
+                    listOut.add(stroka);
                 }
-                list.add(strTemp[0] + " " + strTemp[1] + "\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
         try(BufferedWriter writer = new BufferedWriter(new FileWriter(fileName)))
         {
-            for (int i = 0; i < list.size(); i++) {
-                writer.write(list.get(i));
+            for (int i = 0; i < listOut.size(); i++) {
+                writer.write(listOut.get(i) + "\n");
             }
             } catch (IOException ex) {
             ex.printStackTrace();
