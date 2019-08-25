eb01795d227a37d569c2d5980253564ffd6311a3
Kingl88 <34752938+Kingl88@users.noreply.github.com>
2019-08-25 20:47:13 +0300

    Add files via upload

--- Lesson12/BirthDay.java
+++ Lesson12/BirthDay.java
@@ -1,1 +1,22 @@
+package by.it_academy.Lesson12;
 
+import java.time.*;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class BirthDay {
+    public static void main(String[] args) {
+        birthDay();
+    }
+
+    private static void birthDay() {
+        Locale locale = Locale.getDefault();
+        LocalDate myBirthDay = LocalDate.of(1988, Month.AUGUST, 7);
+        for (int i = 0; i < 81; i++) {
+           String dow = myBirthDay.getDayOfWeek().getDisplayName(TextStyle.FULL, locale);
+            System.out.printf("%s:%s\n", myBirthDay.getYear(), dow.substring(0, 1).toUpperCase() + dow.substring(1));
+           myBirthDay = myBirthDay.plusYears(1);
+
+        }
+    }
+}
